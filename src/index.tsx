30d2a152759fc2dbee90aee2183829a2cacacb6d
Shawn Paul <shawn.paul@genesys.com>
2022-11-06 16:58:19 -0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+      
+    </RecoilRoot>
+    
+  </React.StrictMode>
+);
